b931893cb16203926f899dc53ae9ef6829c600b6
internalinterference <kanequbolt@gmail.com>
2020-10-01 18:03:34 +1000

    first commit

---
+++ API/Data/ICommanderRepo.cs
@@ -0,0 +1,15 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public interface ICommanderRepo
+    {
+        IEnumerable<Command> GetCommands();
+        Command GetCommandById(int id);
+    }
+}
