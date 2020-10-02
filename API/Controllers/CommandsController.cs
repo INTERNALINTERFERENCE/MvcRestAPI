48977613a5151f10d032eb8d5ed810c578a8ca27
internalinterference <kanequbolt@gmail.com>
2020-10-02 17:23:19 +1000

    new controller

---
+++ API/Controllers/CommandsController.cs
@@ -0,0 +1,35 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [Route("api/commands")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        private readonly Repository _repository = new Repository();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Command>> GetAllCommands()
+        {
+            var commandItems = _repository.GetCommands();
+
+            return Ok(commandItems);
+        }
+
+        //Get api/commands/5(example)
+        [HttpGet("{id}")]
+        public ActionResult<Command> GetCommandById(int id)
+        {
+            var commandItem = _repository.GetCommandById(id);
+
+            return Ok(commandItem);
+        }
+    }
+}
---
+++ API/Data/Repository.cs
@@ -0,0 +1,40 @@
+﻿using API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.Data
+{
+    public class Repository : ICommanderRepo
+    {
+        public Command GetCommandById(int id)
+        {
+            return new Command
+            {
+                Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan"
+            };
+        }
+
+        public IEnumerable<Command> GetCommands()
+        {
+            var commands = new List<Command>
+            {
+                new Command
+                {
+                    Id = 0, HowTo = "Boil an egg", Line = "Boil water", Platform = "Kettle & Pan"
+                },
+                new Command
+                {
+                    Id = 1, HowTo = "Cut a bread", Line = "Get a knife", Platform = "knife & shopping board"
+                },
+                new Command
+                {
+                    Id = 2, HowTo = "Make cup of tea", Line = "Place teabag in a cup", Platform = "Kettle & Cup"
+                },               
+            };
+            return commands;
+        }      
+    }
+}
